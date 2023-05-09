ca0e98744166583dbadbe16f1a131eabf947db90
SK-20220630ENDY\Administrator <ciostuben264@gmail.com>
2023-05-09 19:44:24 +0800

    Add FunctionList

--- 功能实现/FunctionRealization/Assets/Common/Editor/CreateCommonUI.cs
+++ 功能实现/FunctionRealization/Assets/Common/Editor/CreateCommonUI.cs
@@ -5,10 +5,10 @@ namespace Common.Editor
 {
     public static class CreateCommonUI
     {
-        [MenuItem("Tool/CommonUI/CreateCommonUI", false, 0)]
-        public static void AddText()
-        {
-            GameObject.Instantiate(Resources.Load("UI/CommonUI"));
-        }
+        // [MenuItem("Tool/CommonUI/CreateCommonUI", false, 0)]
+        // public static void AddText()
+        // {
+        //     GameObject.Instantiate(Resources.Load("UI/CommonUI"));
+        // }
     }
 }
